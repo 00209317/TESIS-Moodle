22bb0da0787e36fef0c5c2bef4e08530235bbfac
luedco <luedcomu@gmail.com>
2022-05-23 22:58:56 -0600

    archivos despues del merge,.

--- version.php
+++ version.php
@@ -30,10 +30,10 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_ecampus';
 
 // This is the version of the plugin.
-$plugin->version = 0000000022;
+$plugin->version = 0000000024;
 
 // This is the named version.
-$plugin->release = '0.0.2.2';
+$plugin->release = '0.0.2.4';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
