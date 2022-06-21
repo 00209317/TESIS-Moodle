57a8ba60ea3f07657a414d1ea2d1c1bfc052b715
luedco <luedcomu@gmail.com>
2022-06-20 21:26:28 -0600

    modal help renders

---
+++ amd/src/modalHelp.js
@@ -0,0 +1,18 @@
+//Activar Edicion
+var ob = document.getElementById("activarEdicion")
+ob.href="../backup/restorefile.php?contextid=3"
+// //Activar Edicion
+// var ob = document.getElementById("activarEdicion")
+// ob.href="../backup/restorefile.php?contextid=3"
+// //Activar Edicion
+// var ob = document.getElementById("activarEdicion")
+// ob.href="../backup/restorefile.php?contextid=3"
+// //Activar Edicion
+// var ob = document.getElementById("activarEdicion")
+// ob.href="../backup/restorefile.php?contextid=3"
+// //Activar Edicion
+// var ob = document.getElementById("activarEdicion")
+// ob.href="../backup/restorefile.php?contextid=3"
+// //Activar Edicion
+// var ob = document.getElementById("activarEdicion")
+// ob.href="../backup/restorefile.php?contextid=3"
