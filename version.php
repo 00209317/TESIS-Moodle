a5ef293c05f036c1cd488b0a941610fa9d59f934
CristianAlfaro <37422663+CristianAlfaro@users.noreply.github.com>
2022-07-21 21:15:42 -0600

    boton flotante

--- version.php
+++ version.php
@@ -30,10 +30,10 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_ecampus';
 
 // This is the version of the plugin.
-$plugin->version = 00000001605;
+$plugin->version = 00000001606;
 
 // This is the named version.
-$plugin->release = '0.1.6.05';
+$plugin->release = '0.1.6.06';
 
 // This is a stable release.s
 $plugin->maturity = MATURITY_STABLE;
